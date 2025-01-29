020d0a76d3e2eb75bb00cf8b1a5a8790bbcbdbb5
Daniil Liplenko <prod408@yandex.ru>
2025-01-29 15:57:08 +0400

    SetUp homeView and modify project structured

--- Hardless/Views/HomeView.swift
+++ Hardless/Views/HomeView.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class HomeView: UIViewController {
-
+    
+    
+    @IBOutlet weak var MainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
         
     }
 
-
+    
+    private func setupUI(){
+        MainLabel.layer.borderWidth = 1
+        MainLabel.layer.borderColor = UIColor(named: "BorderColor")?.cgColor
+        MainLabel.layer.cornerRadius = 10
+        
+    }
 }
 
