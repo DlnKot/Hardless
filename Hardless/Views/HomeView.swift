c02dc9ccd947ceb55958a3dcd9a7162eeda90c2e
Daniil Liplenko <prod408@yandex.ru>
2025-01-29 19:01:39 +0400

    I'm tried, constreints shits...

--- Hardless/Views/HomeView.swift
+++ Hardless/Views/HomeView.swift
@@ -10,21 +10,41 @@ import UIKit
 class HomeView: UIViewController {
     
     
-    @IBOutlet weak var MainLabel: UILabel!
+    @IBOutlet weak var MainLabel: UITextView!
+    
+    @IBOutlet var NumButton: [UIButton]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupUI()
+        setupMainLabel()
         
     }
 
     
-    private func setupUI(){
+    private func setupMainLabel(){
         MainLabel.layer.borderWidth = 1
         MainLabel.layer.borderColor = UIColor(named: "BorderColor")?.cgColor
         MainLabel.layer.cornerRadius = 10
+        MainLabel.textContainerInset = UIEdgeInsets(top: 0, left: 10, bottom: 5, right: 10)
+        MainLabel.text = "254"
+        MainLabel.isScrollEnabled = false
+        MainLabel.isEditable = false
+        MainLabel.isSelectable = false
+        NSLayoutConstraint.activate([
+            MainLabel.widthAnchor.constraint(equalToConstant: 200),
+            MainLabel.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        MainLabel.font = UIFont.systemFont(ofSize: 54)
+        MainLabel.adjustsFontForContentSizeCategory = true
+       
+    }
+    
+    private func setUpNumbers() {
+        for num in NumButton {
+            num.layer.cornerRadius = 25
+        
+        }
         
     }
 }
